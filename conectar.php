065c8d531dd723ca4c8496cea2223d9e8a78e182
DiegoPercast <nmadara87@gmail.com>
2023-11-28 00:13:20 -0700

    second commit

--- conectar.php
+++ conectar.php
@@ -3,5 +3,14 @@
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'teatrodejavu');
-    echo "el pene";
+
+
+    /* Se hace la conexion a la base de datos */
+    $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    
+    // Se revisa la conexion
+    if($mysqli === false){
+        die("ERROR: No se ha podido conectar. " . $mysqli->connect_error);
+    }
+
 ?>
