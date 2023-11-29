5a0c9d75661c447e2ddaa6838b6f000a049261af
Becerron <21325050510593@cbtis051.edu.mx>
2023-11-29 02:40:37 -0700

    yes, sir i can boogey

--- account.php
+++ account.php
@@ -4,7 +4,7 @@ $login = '
 <div id="login">
 <img src="assets/img/logos/rojo.png" alt="logo" class="logoform">
 <div class="Titulo">Iniciar sesión</div>
-<form action="#" class="form">
+<form action="account.php" class="form" method="POST">
     <input type="email" name="nombrecorreo" id="nombrecorreo" placeholder="Correo electronico" required>
     <input type="password" name="contra" id="contra" placeholder="Contraseña" required>
     <input type="submit" name="submit" class="enviar" value="Iniciar sesión">
@@ -19,8 +19,8 @@ $admin = '
 <div id="login">
 <img src="assets/img/logos/rojo.png" alt="logo" class="logoform">
 <div class="Titulo">Iniciar sesion (Admin)</div>
-<form action="#" class="form">
-    <input type="email" name="nombrecorreo" id="nombrecorreo" placeholder="Correo electronico" required>
+<form action="account.php" class="form" method="POST">
+  <input type="email" name="correo" id="nombrecorreo" placeholder="Correo electronico" required>
     <input type="password" name="contra" id="contra" placeholder="Contraseña" required>
     <input type="submit" name="submit" class="enviar" value="Iniciar sesión">
     <input type="hidden" name="typeuser" value="admin">
@@ -33,7 +33,7 @@ $signup = '
 <div id="signup">
     <img src="assets/img/logos/rojo.png" alt="logo" class="logoform">
     <div class="Titulo">Crea una cuenta</div>
-    <form action="#" class="form">
+    <form action="account.php" class="form" method="POST">
         <input type="email" name="correo" id="nombrecorreo" placeholder="Correo electronico" required>
         <input type="text" name="nombre" id="nombrecorreo" placeholder="Nombre completo" required>
         <input type="password" name="contra" id="contra" placeholder="Contraseña" required>
@@ -66,6 +66,8 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
       echo $signup;
     }elseif ($_GET['tipo']=='admin') {
       echo $admin;
+    }elseif ($_GET['tipo']=='logout'){
+      header("location: index.php");
     }
 
   } else {
@@ -79,5 +81,72 @@ echo '
 ';
 
 //Funciones de inicio de sesion
+//Inicio para los administradores
+if($_SERVER['REQUEST_METHOD']=='POST'){
+  if($_POST['typeuser']=='admin'){
 
+    $err_correo = "";
+    $err_contra = "";
+    
+  
+    $input_correo = trim($_POST["correo"]);
+      if(empty($input_correo)){
+          $err_correo = "Introduce un correo valido.";
+      } elseif(!filter_var($input_correo, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z0-9ñÑ@.\s]+$/")))){
+          $err_correo = "Introduce un correo valido.";
+      } else{
+          $correo = $input_correo;
+      }
+      //validacion del numero
+      $inputcontra = trim($_POST["contra"]);
+      if(empty($inputcontra)){
+          $err_contra = "Introduce una contraseña valida.";
+      } elseif(!filter_var($inputcontra, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-ZñÑ0-9\s]+$/")))){
+          $err_contra = "Introduce un contraseña valida.";
+      } else{
+          $contra = $inputcontra;
+      }
+  
+    require 'conectar.php';
+    if(empty($err_contra) && empty($err_correo)){
+    
+      $sql = "SELECT * FROM administradores WHERE correo ='".$correo."'";
+      if($result = $mysqli->query($sql)){
+          // se comprueba si existen registros con el nombre
+          if($result->num_rows > 0){
+              while($row = $result->fetch_array()){
+                  $contratabla = $row['contra'];
+              
+              }
+              if($contratabla == $contra){
+                  echo '
+                  <form action="administrador.php" id="sesioniniciada" method="POST">
+                      <input type="hidden" name="inicio" value="simon">                   
+                  </form>
+                  <script>document.getElementById("sesioniniciada").submit();</script>
+                  ';
+              }else{
+                  $err = "Contraseña incorrecta";
+                  header("location: account.php?err=".$err);
+              }
+              // Se limpia el registro del resultado
+              $result->free();
+          } else{
+              // 
+              $err = "No se ha encontrado este usuario, vuelve a intentarlo";
+              header("location: account.php?err=".$err);
+          }
+      } else{
+          $err = "Algo en la conexion está mal";
+      }
+
+      // Cerrar la conexcion
+      $mysqli->close();
+    }else{
+      $err = "El usuario o contraseña esta mal";
+      header("location: account.php?err=".$err);
+    
+    }
+  }
+}
 ?>
--- administrador.php
+++ administrador.php
@@ -1,5 +1,131 @@
 <?php
-if($_SERVER['REQUEST_METHOD']=='POST'){
+$paginaadmin = '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <title>Modo Admin</title>
+    <!-- Estilos propios de la pagina-->
+    
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="./assets/img/logos/rojo.png" />
+    <!-- Font Awesome icons (free version)-->
+    <script
+      src="https://use.fontawesome.com/releases/v6.4.2/js/all.js"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Google fonts-->
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:400,700"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <!--Main Styles-->
+    <link rel="stylesheet" href="./css/admin.css">
+    <link rel="stylesheet" href="./css/styles.css" />
+    <link rel="stylesheet" href="./css/boletos.css" />
+  </head>
+  <body id="page-top">
+    <!--Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="#"
+          ><img src="assets/img/logos/rojo.png" alt="..." class="icon"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="account.php?tipo=logout">Cerrar sesión</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--Main-->
+    <div class="containeradmin">
+      <h1 class="title">Administrar teatro</h1>
+      <div id="adminsections">
+        <div id="sec1" class="adminsection">
+          <div class="titlesec">Venta de boletos</div>
+          <div class="listadmin">
+            <div class="listelement" style="margin-top: 12px;">
+              <img src="assets/img/icons/calendario.png" alt="calendario" class="iconadmin">
+              <h5>Administrar eventos</h3>
+              </div>
+              <div class="listelement">
+                <img src="assets/img/icons/teatro.png" alt="mascaras" class="iconadmin">
+                <h5>Administrar obras</h3>
+                </div>
+                <div class="listelement">
+              <img src="assets/img/icons/butacas-de-cine.png" alt="butacas" class="iconadmin">
+              <h5>Administrar salas</h5>
+            </div>
+          </div>
+        </div>
+        <div id="sec2" class="adminsection">
+          <div class="titlesec">Academia</div>
+          <div class="listadmin">
+            <div class="listelement" style="margin-top: 12px;">
+              <img src="assets/img/icons/graduado.png" alt="butacas" class="iconadmin">
+              <h5>Administrar alumnos</h5>
+              
+            </div>
+            <div class="listelement">
+              <img src="assets/img/icons/graduacion.png" alt="butacas" class="iconadmin">
+              <h5>Administrar cursos</h5>
+              
+            </div>
+            <div class="listelement">
+              <img src="assets/img/icons/cine.png" alt="butacas" class="iconadmin">
+              <h5>Administrar salas</h5>
 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="./js/boletos.js"></script>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+  </body>
+</html>
+';
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    
+    if($_POST['inicio']=='simon'){
+        echo $paginaadmin;
+    }else{
+        header("location: account.php?err=Es%20necesario%20iniciar%20sesion%20para%20acceder%20comoa%20dministrador");
+    }
+}else{
+    header("location: account.php?err=Es%20necesario%20iniciar%20sesion%20para%20acceder%20comoa%20dministrador");
 }
 ?>
